288119164484a6ea9cd9c7c4c1b0070b9fc77af7
vash <1805090789@qq.com>
2020-04-23 18:43:29 +0800

    add：order项目，注册到Eureka Service，同时调用Eureka中item服务 1.添加依赖 2.修改yml，添加注册信息 3.启动类增加@EnableEurekaClient，RestTemplate增加@LoadBalanced 4.修改调用方法中url和参数

--- microservice-order/src/main/java/com/vash/order/OrderApplication.java
+++ microservice-order/src/main/java/com/vash/order/OrderApplication.java
@@ -2,9 +2,12 @@ package com.vash.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderApplication {
 
@@ -13,6 +16,7 @@ public class OrderApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
