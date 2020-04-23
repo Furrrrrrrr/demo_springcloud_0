a8d95a79f288da319aff91ff131c21429dd11127
vash <1805090789@qq.com>
2020-04-23 19:24:34 +0800

    add：搭建Eureka Server集群 添加Eureka Server - 1工程 1.yml中注册及获取注册信息改为true 2.客户端调用地址改为其他server地址

---
+++ eureka-server-1/src/test/java/com/vash/eurekaserver1/EurekaServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vash.eurekaserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
