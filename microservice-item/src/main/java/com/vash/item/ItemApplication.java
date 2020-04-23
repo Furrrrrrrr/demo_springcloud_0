a83d55c632cd61a54669629b0734d328114bc7ca
vash <1805090789@qq.com>
2020-04-23 18:44:35 +0800

    Revert "add：item项目，注册到Eureka Service"
    This reverts commit 07efe576

--- microservice-item/src/main/java/com/vash/item/ItemApplication.java
+++ microservice-item/src/main/java/com/vash/item/ItemApplication.java
@@ -2,9 +2,7 @@ package com.vash.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class ItemApplication {
 
